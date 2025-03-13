c41fc0c81695e88abeeac928bc77b7400af346a7
Anil Bagale <121026223+anilbagale@users.noreply.github.com>
2025-03-14 00:14:14 +0530

    Update Install Latest Dialpad.sh

--- Install Latest Dialpad.sh
+++ Install Latest Dialpad.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-Dialpad InstallerIT
------------------------------------------------------------------------
-	Written by: Anil Bagale
-
-	
-	Created: February 08, 2021
-        Updated: February 08, 2021
-	Purpose: Downloads and installs the latest available Dialpad
-	app specified directly on the client. This avoids having to
-	manually download and store an up-to-date installer on a
-	distribution server every month.
-	
------------------------------------------------------------------------
-ABOUT_THIS_SCRIPT
-
 
 # this is the full URL
 url="http://storage.googleapis.com/dialpad_native/osx/dialpad_dist.pkg"
